9bbcae5355317cbf68f8ff4e758e2d48822a58cf
Justin Huff <jjhuff@mspin.net>
2023-10-28 16:06:23 -0700

    Mitsubishi Minisplit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Various ESPHome Projects
+
+1. Create a `secrets.yaml` file with your `wifi_ssid` and `wifi_password`.
+2. Attach your Inkplate to a USB port
+3. `./esphome.sh run <file>.yaml --device=/dev/ttyUSB0`
---
+++ esphome.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+ARGS="-p 6052:6052"
+
+if [ -c "/dev/ttyUSB0" ]; then
+  ARGS+=" --device /dev/ttyUSB0"
+fi
+
+if [ -c "/dev/ttyACM0" ]; then
+  ARGS+=" --device /dev/ttyACM0"
+fi
+
+docker run --rm -it  -v "${PWD}":/config $ARGS esphome/esphome:2023.10.3 $@
