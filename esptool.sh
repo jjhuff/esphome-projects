d380401ddad670c1be1a4c8ced9ee9aadc2971fd
Justin Huff <jjhuff@mspin.net>
2025-10-19 20:58:55 -0700

    misc

---
+++ esptool.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+ARGS="-p 6052:6052"
+
+if [ -c "/dev/ttyUSB0" ]; then
+  ARGS+=" --device /dev/ttyUSB0"
+fi
+
+if [ -c "/dev/ttyACM0" ]; then
+  ARGS+=" --device /dev/ttyACM0"
+fi
+
+docker run --rm -it  -v "${PWD}":/config $ARGS --entrypoint=esptool.py esphome/esphome:2024.10.3 $@
